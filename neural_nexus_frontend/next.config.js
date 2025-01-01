4f413e0af58f57dd78eed18e84e04bca860789bb
WTWalters <whitney.walters@gmail.com>
2025-01-01 11:01:01 -0700

    updated next.congig.js for front end

--- neural_nexus_frontend/next.config.js
+++ neural_nexus_frontend/next.config.js
@@ -1,6 +1,17 @@
-/** @type {import("next").NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: "standalone",
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
