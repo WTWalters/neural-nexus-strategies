50464d7f661c276d392a2486268022891d3b1b3c
WTWalters <whitney.walters@gmail.com>
2025-01-06 09:25:36 -0700

    update for issues in production

--- neural_nexus_frontend/next.config.js
+++ neural_nexus_frontend/next.config.js
@@ -1,45 +1,20 @@
-// Path: neural_nexus_frontend/next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
-  async rewrites() {
-    async rewrites() {
-      return [{
+  rewrites: async () => {
+    return [
+      {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`, // Remove /api from here
-      }];
-    }
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+      },
+    ];
   },
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   images: { unoptimized: true },
-  webpack: (config) => {
-    config.optimization = {
-      ...config.optimization,
-      splitChunks: {
-        chunks: "all",
-        cacheGroups: {
-          framework: {
-            test: /[\\/]node_modules[\\/](react|react-dom|next)[\\/]/,
-            name: "framework",
-            chunks: "all",
-            priority: 40,
-          },
-          commons: {
-            test: /[\\/]node_modules[\\/]/,
-            name: "commons",
-            chunks: "all",
-            minChunks: 2,
-            priority: 20,
-          },
-        },
-      },
-    };
-    return config;
-  },
 };
 
 module.exports = nextConfig;
